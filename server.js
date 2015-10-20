6e27bc8c82a291866c285aaf193d6d400df66eb5
Tyler Murphy <tmurphy@roirevolution.com>
2015-10-19 20:14:34 -0400

    Allows adding users to the right-hand column for tracking their text.

--- server.js
+++ server.js
@@ -6,7 +6,15 @@ var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 var home_directory = process.env.OPENSHIFT_HOMEDIR || __dirname;
 
-var users = [];
+var users = {
+	user1: { text: 'Once upon a time there was a bunny.' },
+	user2: { text: 'Once upon a time Mikaela was a dummy.' }
+/*
+	<username>: {
+		text: 'Once upon a time'
+	}
+*/
+};
 
 var server = http.createServer(function (request, response) {
 	fs.read(home_directory + '/index.html').then(function(file) {
@@ -25,15 +33,19 @@ io.on('connection', function(socket) {
 	console.log('someone connected');
 
 	socket.on('add user', function(username) {
-		if (users.indexOf(username) !== -1) {
+		if (username in users) {
 			socket.emit('login failed', { reason: 'username taken... try a different one' });
 			return;
 		}
 		socket.username = username;
-		socket.emit('login', { users: users });
-		users.push(username);
+		socket.emit('login', { users: Object.keys(users) });
+		users[username] = { text: '' };
 		userAdded = true;
 		socket.broadcast.emit('new user', { user: username });
+	});
+
+	socket.on('get other text', function(username) {
+		socket.emit('got other text', { username: username, text: users[username].text });
 	});
 
 	socket.on('character', function(character) {
@@ -42,6 +54,7 @@ io.on('connection', function(socket) {
 			user: socket.username,
 			character: character
 		});
+		users[socket.username].text += character;
 	});
 
 
