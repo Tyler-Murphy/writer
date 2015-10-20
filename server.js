70ea827d6b33365d96f1b49525822b85e8d65423
Tyler Murphy <tmurphy@roirevolution.com>
2015-10-19 22:12:16 -0400

    Fixes the openshift home directory.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var socketIo = require('socket.io');
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
-var home_directory = process.env.OPENSHIFT_HOMEDIR || __dirname;
+var home_directory = process.env.OPENSHIFT_REPO_DIR || __dirname;
 
 var users = {
 /*
